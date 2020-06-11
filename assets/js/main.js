65f6b443c2e9144607c8440aea7a1f1a3dbafef9
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-06-11 13:22:09 +0600

    added about us and our courses

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 $('.carousel').carousel({
   interval: 5000
-})
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+     autoplay: true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
