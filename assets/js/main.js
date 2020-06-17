52aa18e9ca8c1a0a87f2b6f6b8b5b005677a1f66
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-06-17 22:23:31 +0600

    Facts and News Section Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,12 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+
+$(document).ready(function(){
+    $('.counter').counterUp({
+      delay: 10,
+      time: 2000
+    });
+    $('.counter').addClass('animated fadeInDownBig');
+    $('h3').addClass('animated fadeIn');
+});
